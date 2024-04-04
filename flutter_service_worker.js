3a0d185999297f479560383b6e5b3a35731543bc
derek-munday <derek-munday@users.noreply.github.com>
2024-04-04 21:14:03 +0000

    deploy: 52c6210d8d25c6652c137420391c3ba677e51f29

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "4599e97a1a8d04cf43c279cc3d4595ba",
-"/": "4599e97a1a8d04cf43c279cc3d4595ba",
+const RESOURCES = {"index.html": "e99ec374ec4db019b8ed7b2a6b0e8fe9",
+"/": "e99ec374ec4db019b8ed7b2a6b0e8fe9",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
